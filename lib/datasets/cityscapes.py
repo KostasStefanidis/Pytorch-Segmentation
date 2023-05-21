ffa23c40f43a2a76c3068b3558c95feb95651922
kstef <kstefanidis48@gmail.com>
2023-05-21 21:46:15 +0000

    Cityscapes dataset with transforms v2 for augmentation and pre-processing

--- lib/datasets/cityscapes.py
+++ lib/datasets/cityscapes.py
@@ -4,9 +4,6 @@ import lightning.pytorch as pl
 from torchvision.datasets import Cityscapes
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from torchvision.transforms import Resize, RandomCrop, RandomHorizontalFlip
-from torchvision.transforms import AugMix, RandAugment, RandomAutocontrast, AutoAugment
-from torchvision.transforms import RandomHorizontalFlip, RandomResizedCrop, RandomAdjustSharpness
 from typing import Tuple, List, Optional, Callable, Any, Union
 from PIL import Image
 from torchvision.models import RegNet_Y_16GF_Weights, RegNet_Y_32GF_Weights
@@ -15,11 +12,11 @@ from torchvision.models import EfficientNet_V2_M_Weights, EfficientNet_V2_S_Weig
 from torchvision.models import MobileNet_V3_Large_Weights, ResNet50_Weights, ResNet101_Weights
 from torchvision import datapoints
 import torchvision.transforms.v2 as transformsv2
+from lib.utils.augmentation import get_augmentations
 
 _IGNORE_IDS = [-1,0,1,2,3,4,5,6,9,10,14,15,16,18,29,30]
 _EVAL_IDS =   [7,8,11,12,13,17,19,20,21,22,23,24,25,26,27,28,31,32,33]
 _TRAIN_IDS =  [0,1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18]
-
 
 class EvalToTrainIds():
     def __init__(self, 
@@ -55,6 +52,19 @@ class OneHot():
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(channels={self.channels})"
 
+DEFAULT_TARGET_TRANSFORM = lambda num_classes: transformsv2.Compose(
+    [
+        EvalToTrainIds(),
+        OneHot(num_classes)
+    ]
+)
+
+DEFAULT_TRANSFORM = transformsv2.Compose(
+    [
+        transformsv2.ConvertImageDtype(torch.float32),
+        #Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    ]
+)
 
 class CityscapesTestSplit(Cityscapes):
     def __init__(self, 
@@ -116,7 +126,7 @@ class CityscapesDataset(Cityscapes):
         
         image = Image.open(self.images[index]).convert("RGB")
 
-        targets: Any = []
+        targets = []
         for i, t in enumerate(self.target_type):
             if t == "polygon":
                 target = self._load_json(self.targets[index][i])
@@ -133,78 +143,84 @@ class CityscapesDataset(Cityscapes):
         if self.augmentations is not None:
             image, target = self.augmentations(image, target)
         
-        image, target = self.transform(image), self.target_transform(target)
+        if self.transform is not None:
+            image = self.transform(image)
+            
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+            
         return image, target
 
 
 class CityscapesDataModule(pl.LightningDataModule):
-    def __init__(self, dataset_config) -> None:
+    def __init__(self, 
+                 dataset_config: dict, 
+                 augmentation_config: dict,
+                 transform: Callable[..., Any] | None = None,
+                 target_transform: Callable[..., Any] | None = None,
+                 ) -> None:
+        
         super().__init__()
         self.datapath = dataset_config['path']
         self.mode = dataset_config.get('mode', 'fine')
         self.target_type = dataset_config.get('type', 'semantic')
         self.num_classes = dataset_config.get('num_classes', 20)
         self.batch_size = dataset_config.get('batch_size', 3)
-        self.target_transform = transforms.Compose(
-            [
-                transforms.PILToTensor(),
-                EvalToTrainIds(),
-                OneHot(self.num_classes)
-            ]
-        )
-        self.transform = transforms.Compose(
-            [
-                transforms.ToTensor(),
-                #Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-            ]
-        )
+        self.num_workers = dataset_config.get('num_workers', 4)
+        if target_transform is not None:
+            self.target_transform = target_transform  
+        else: 
+            self.target_transform = DEFAULT_TARGET_TRANSFORM(self.num_classes)
+            
+        self.transform = transform if transform is not None else DEFAULT_TRANSFORM
+        
+        self.augmentations = get_augmentations(augmentation_config)
         
         
     def setup(self, stage: str) -> None:
         if stage == 'fit':
-            self.train_ds = Cityscapes(root=self.datapath, 
-                                       split='train', 
-                                       mode=self.mode,
-                                       target_type=self.target_type,
-                                       transform=self.transform,
-                                       target_transform=self.target_transform)
+            self.train_ds = CityscapesDataset(root=self.datapath, 
+                                             split='train', 
+                                             mode=self.mode,
+                                             target_type=self.target_type,
+                                             transform=self.transform,
+                                             target_transform=self.target_transform,
+                                             augmentations=self.augmentations)
             
-            self.val_ds = Cityscapes(root=self.datapath, 
-                                     split='val', 
-                                     mode=self.mode,
-                                     target_type=self.target_type,
-                                     transform=self.transform,
-                                     target_transform=self.target_transform)
+            self.val_ds = CityscapesDataset(root=self.datapath, 
+                                             split='val', 
+                                             mode=self.mode,
+                                             target_type=self.target_type,
+                                             transform=self.transform,
+                                             target_transform=self.target_transform)
         
         if stage == 'test':
-            self.test_ds = Cityscapes(root=self.datapath, 
-                                      split='val', 
-                                      mode=self.mode,
-                                      target_type=self.target_type,
-                                      transform=self.transform,
-                                      target_transform=self.target_transform)
+            self.test_ds = CityscapesDataset(root=self.datapath, 
+                                             split='val', 
+                                             mode=self.mode,
+                                             target_type=self.target_type,
+                                             transform=self.transform,
+                                             target_transform=self.target_transform)
         
         if stage == 'predict':
             self.predict_ds = CityscapesTestSplit(root=self.datapath,
                                                   transform=self.transform)
     
     def train_dataloader(self):
-        return DataLoader(self.train_ds, self.batch_size, shuffle=True, num_workers=4)
+        return DataLoader(self.train_ds, self.batch_size, shuffle=True, num_workers=self.num_workers)
     
     def val_dataloader(self):
-        return DataLoader(self.val_ds, self.batch_size, shuffle=False, num_workers=0)
+        return DataLoader(self.val_ds, self.batch_size, shuffle=False, num_workers=self.num_workers)
     
     def test_dataloader(self):
-        return DataLoader(self.test_ds, self.batch_size, shuffle=False, num_workers=4)
+        return DataLoader(self.test_ds, self.batch_size, shuffle=False, num_workers=self.num_workers)
         
     def predict_dataloader(self):
-        return DataLoader(self.predict_ds, self.batch_size, shuffle=False, num_workers=4)
+        return DataLoader(self.predict_ds, self.batch_size, shuffle=False, num_workers=self.num_workers)
 
 
 def get_preprocessing(self, backbone, weight_version):
-    weights_str = f'{backbone}_Weights.{weight_version}' 
-    # Layer for normalizing input image
-    #default_normalization_layer = tf.keras.layers.Rescaling(scale=1./127.5, offset=-1)
+
     preprocessing_options = {
         'default': transforms.ToTensor(), # Convert to Tensor and
         #'ResNet': resnet.preprocess_input,
