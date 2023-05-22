23f8c7801896877b00d73f9a4ea45bd23e2d826e
Konstantinos Stefanidis <100881886+KostasStefanidis@users.noreply.github.com>
2023-05-22 14:04:01 +0300

    Update predict.py to use main() function

--- predict.py
+++ predict.py
@@ -6,46 +6,50 @@ from argparse import ArgumentParser
 import yaml
 import os
 
-parser = ArgumentParser('')
-parser.add_argument('--config', type=str, nargs='?')
-args = parser.parse_args()
+def main():
+    parser = ArgumentParser('')
+    parser.add_argument('--config', type=str, nargs='?')
+    args = parser.parse_args()
 
-with open(args.config, 'r') as config_file:
-    config = yaml.safe_load(config_file)
+    with open(args.config, 'r') as config_file:
+        config = yaml.safe_load(config_file)
+
+        LOGS_DIR = config.get('logs_dir')
+
+        dataset_config = config.get('dataset')
+        model_config = config.get('model')
+        inference_config = config.get('inference_config')
+
+        DATASET = dataset_config.get('name')
+        DATA_PATH = dataset_config.get('path')
+        VERSION = dataset_config['version']
+
+        MODEL_TYPE = model_config.get('architecture')
+        MODEL_NAME = model_config.get('name')
+        BACKBONE = model_config.get('backbone')
+
+        INFERENCE_OUTPUT_STRIDE = inference_config.get('output_stride')
+        INFERENCE_PRECISION = inference_config.get('precision')
+
+    torch.set_float32_matmul_precision(INFERENCE_PRECISION)
+
+    checkpoint_dir = f'{LOGS_DIR}/saved_models/{MODEL_TYPE}/{MODEL_NAME}.ckpt'
+    grayscale_path = f'{LOGS_DIR}/predictions/{MODEL_TYPE}/{MODEL_NAME}/test/grayscale'
+    rgb_path = f'{LOGS_DIR}/predictions/{MODEL_TYPE}/{MODEL_NAME}/test/rgb'
+
+    os.makedirs(grayscale_path, exist_ok=True)
+    os.makedirs(rgb_path, exist_ok=True)
+
+    data_module = CityscapesDataModule(dataset_config=dataset_config)
+
+    model = SegmentationModule.load_from_checkpoint(checkpoint_dir)
+
+    trainer = pl.Trainer(
+        accelerator='gpu',
+        devices='0',
+    )
+
+    trainer.predict(model, datamodule=data_module, return_predictions=False)
     
-    LOGS_DIR = config['logs_dir']
-
-    dataset_config = config['dataset']
-    model_config = config['model']
-    inference_config = config['inference_config']
-    
-    DATASET = dataset_config['name']
-    DATA_PATH = dataset_config['path']
-    VERSION = dataset_config['version']
-    
-    MODEL_TYPE = model_config['architecture']
-    MODEL_NAME = model_config['name']
-    BACKBONE = model_config['backbone']
-    
-    INFERENCE_OUTPUT_STRIDE = inference_config['output_stride']
-    INFERENCE_PRECISION = inference_config['precision']
-
-torch.set_float32_matmul_precision(INFERENCE_PRECISION)
-
-checkpoint_dir = f'{LOGS_DIR}/saved_models/{MODEL_TYPE}/{MODEL_NAME}.ckpt'
-grayscale_path = f'{LOGS_DIR}/predictions/{MODEL_TYPE}/{MODEL_NAME}/test/grayscale'
-rgb_path = f'{LOGS_DIR}/predictions/{MODEL_TYPE}/{MODEL_NAME}/test/rgb'
-
-os.makedirs(grayscale_path, exist_ok=True)
-os.makedirs(rgb_path, exist_ok=True)
-
-data_module = CityscapesDataModule(dataset_config=dataset_config)
-
-model = SegmentationModule.load_from_checkpoint(checkpoint_dir)
-
-trainer = pl.Trainer(
-    accelerator='gpu',
-    devices='0',
-)
-
-trainer.predict(model, datamodule=data_module, return_predictions=False)
+if __name__ == '__main__':
+    main()
