3469e428d158c2e449c362811a603a28fa914b97
kstef <kstefanidis48@gmail.com>
2023-05-29 13:13:36 +0000

    Update predict.py

--- predict.py
+++ predict.py
@@ -9,35 +9,34 @@ import os
 def main():
     parser = ArgumentParser('')
     parser.add_argument('--config', type=str, nargs='?')
+    parser.add_argument('--logs_dir', type=str, default='/mnt/logs')
+    parser.add_argument('--dataset_config', type=str, default='config/dataset.yaml')
     args = parser.parse_args()
 
     with open(args.config, 'r') as config_file:
-        config = yaml.safe_load(config_file)
+        config: dict = yaml.safe_load(config_file)
+    
+    with open(args.dataset_config, 'r') as dataset_config_file:
+        dataset_config: dict = yaml.safe_load(dataset_config_file).get('dataset_config')
+    
+    logs_dir = args.logs_dir
+    model_config: dict = config.get('model_config')
 
-        LOGS_DIR = config.get('logs_dir')
+    MODEL_TYPE = model_config.get('architecture')
+    MODEL_NAME = model_config.get('name')
 
-        dataset_config = config.get('dataset_config')
-        model_config = config.get('model_config')
-        inference_config = config.get('inference_config')
-
-        MODEL_TYPE = model_config.get('architecture')
-        MODEL_NAME = model_config.get('name')
-
-        INFERENCE_OUTPUT_STRIDE = inference_config.get('output_stride', 16)
-        INFERENCE_PRECISION = inference_config.get('precision')
-
-    torch.set_float32_matmul_precision(INFERENCE_PRECISION)
-
-    checkpoint_dir = f'{LOGS_DIR}/saved_models/{MODEL_TYPE}/{MODEL_NAME}.ckpt'
-    grayscale_path = f'{LOGS_DIR}/predictions/{MODEL_TYPE}/{MODEL_NAME}/test/grayscale'
-    rgb_path = f'{LOGS_DIR}/predictions/{MODEL_TYPE}/{MODEL_NAME}/test/rgb'
+    checkpoint_dir = f'{logs_dir}/saved_models/{MODEL_TYPE}/{MODEL_NAME}.ckpt'
+    grayscale_path = f'{logs_dir}/predictions/{MODEL_TYPE}/{MODEL_NAME}/test/grayscale'
+    rgb_path = f'{logs_dir}/predictions/{MODEL_TYPE}/{MODEL_NAME}/test/rgb'
 
     os.makedirs(grayscale_path, exist_ok=True)
     os.makedirs(rgb_path, exist_ok=True)
 
+    torch.set_float32_matmul_precision('high')
+
     data_module = CityscapesDataModule(dataset_config=dataset_config)
 
-    model = SegmentationModule.load_from_checkpoint(checkpoint_dir, logs_dir=LOGS_DIR)
+    model = SegmentationModule.load_from_checkpoint(checkpoint_dir, logs_dir=logs_dir)
 
     trainer = pl.Trainer(
         accelerator='gpu',
